4adf7baeb7c5b1b59edb6d154132fbfb76b99c12
Ivanpepp <nastroivan3@gmail.com>
2022-01-06 14:42:06 +0100

    alert implementated

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,98 +1,36 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-
-   
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+    <div>
+      <h1>{{Asks[indexCounter].request}}</h1>
+      <ul>
+        <li @click="isCurrect(Asks[indexCounter].answer1)"   class="btn btn-secondary">{{Asks[indexCounter].answer1}}</li>
+        <li @click="isCurrect(Asks[indexCounter].answer2)"    class="btn btn-secondary">{{Asks[indexCounter].answer2}}</li>
+        <li @click="isCurrect(Asks[indexCounter].answer3)"    class="btn btn-secondary">{{Asks[indexCounter].answer3}}</li>
+        <li @click="isCurrect(Asks[indexCounter].answer4)"    class="btn btn-secondary">{{Asks[indexCounter].answer4}}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+
+import Asks from '../data/ask.js'
 export default {
   name: "HelloWorld",
-  props: {
-    msg: String,
+  data: function(){
+    return{
+      Asks,
+      indexCounter: 0,
+    }
+  },
+  methods:{
+    isCurrect: function(answer){
+      if(answer == this.Asks[this.indexCounter].exact){
+        alert('risposta esatta');
+      }else{
+        alert('risposta sbagliata');
+      }
+    }
   },
 };
 </script>
---
+++ src/data/ask.js
@@ -0,0 +1,28 @@
+export default[
+    {
+        "request": "come ti chiami?",
+        "answer1": "luca",
+        "answer2": "paolo",
+        "answer3": "ivan",
+        "answer4": "ernesto",
+        'exact' : 'ivan',
+        
+      },
+      {
+        "request": "quanti anni hai",
+        "answer-1": "10",
+        "answer-2": "12",
+        "answer-3": "1900",
+        "answer-4": "27",
+        'exact' : 'ivan',
+        
+      },
+      {
+        "request": "da dove vieni",
+        "answer-1": "zambia",
+        "answer-2": "napoli",
+        "answer-3": "zurigo",
+        "answer-4": "jotunheim",
+        'exact' : 'napoli',
+      },
+]
